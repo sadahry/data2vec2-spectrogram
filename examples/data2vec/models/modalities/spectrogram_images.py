94e896ac88fba92d2763c3a1e368240ab20f1515
sadahry <sadahiroyoshi@gmail.com>
2023-03-26 19:28:37 +0000

    add images spectrogram code (in progress)

---
+++ examples/data2vec/models/modalities/spectrogram_images.py
@@ -0,0 +1,310 @@
+# Based on https://github.com/facebookresearch/fairseq/blob/d871f6169f8185837d1c11fb28da56abfd83841c/examples/data2vec/models/modalities/images.py
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from functools import partial
+from dataclasses import dataclass, field
+from typing import Callable, Dict, Optional
+from fairseq.tasks import FairseqTask
+from .base import (
+    D2vModalityConfig,
+    ModalitySpecificEncoder,
+    get_alibi_bias,
+    MaskSeed,
+)
+from fairseq.modules import (
+    LayerNorm,
+    SamePad,
+    TransposeLast,
+)
+from .modules import (
+    BlockEncoder,
+    Decoder1d,
+)
+from examples.data2vec.data.modality import Modality
+
+
+@dataclass
+class D2vSpectrogramConfig(D2vModalityConfig):
+    type: Modality = Modality.SPECTROGRAM
+
+    patch_channel_dim: int = field(
+        default=128,
+        metadata={
+            "help": "number of patch_channel_dim when reshaping spectrogram input"
+        },
+    )
+    patch_size: int = field(
+        default=2,
+        metadata={"help": "number of patch_size when reshaping spectrogram input"},
+    )
+
+    embed_dim: int = 768
+
+    alibi_dims: int = 2
+    alibi_distance: str = "manhattan"
+
+    fixed_positions: bool = True
+
+    transformer_decoder: bool = False
+    enc_dec_transformer: bool = False
+
+    # TODO Needs ablation study. These settings are bollowed from https://github.com/facebookresearch/fairseq/blob/d871f6169f8185837d1c11fb28da56abfd83841c/examples/data2vec/models/modalities/audio.py#L35-L46
+    conv_pos_width: int = field(
+        default=95,
+        metadata={"help": "number of filters for convolutional positional embeddings"},
+    )
+    conv_pos_groups: int = field(
+        default=16,
+        metadata={"help": "number of groups for convolutional positional embedding"},
+    )
+    conv_pos_depth: int = field(
+        default=5,
+        metadata={"help": "depth of positional encoder network"},
+    )
+
+
+class SpectrogramPatchEmbed(nn.Module):
+    """2D Spectrogram to Patch Embedding"""
+
+    def __init__(
+        self,
+        patch_size=2,
+        patch_channel_dim=128,
+        embed_dim=768,
+        bias=True,
+    ):
+        super().__init__()
+        self.proj = nn.Conv2d(
+            1,
+            embed_dim,
+            kernel_size=(patch_channel_dim, patch_size),
+            stride=(patch_channel_dim, patch_size),
+            bias=bias,
+        )
+        self.norm = nn.LayerNorm(embed_dim)
+        self.patch_channel_dim = patch_channel_dim
+
+    def forward(self, x):
+        assert len(x.shape) == 3, "must be Batch,Time,Channel"
+        assert (
+            x.size(-1) % self.patch_channel_dim == 0
+        ), "must be divided by patch_channel_dim"
+
+        x = x.unsqueeze(1).transpose(-1, -2)
+        # faster logic for Spectrogram
+        x = self.proj(x).squeeze(2).transpose(-1, -2)
+        # x = self.proj(x).transpose(-1, -2)
+        # if self.flatten:
+        #     x = x.flatten(2).transpose(1, 2)  # B1CT -> BTC
+        x = self.norm(x)
+        return x
+
+
+class SpectrogramEncoder(ModalitySpecificEncoder):
+
+    modality_cfg: D2vSpectrogramConfig
+
+    def __init__(
+        self,
+        modality_cfg: D2vSpectrogramConfig,
+        embed_dim: int,
+        make_block: Callable[[float, Optional[int], Optional[int]], nn.ModuleList],
+        norm_layer: Callable[[int], nn.LayerNorm],
+        layer_norm_first: bool,
+        alibi_biases: Dict,
+        task: Optional[FairseqTask],
+    ):
+
+        local_encoder = SpectrogramPatchEmbed(
+            modality_cfg.patch_size,
+            modality_cfg.patch_channel_dim,
+            modality_cfg.embed_dim,
+        )
+
+        w = local_encoder.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        if modality_cfg.embed_dim != embed_dim:
+            local_encoder = nn.Sequential(
+                local_encoder,
+                nn.Linear(modality_cfg.embed_dim, embed_dim),
+            )
+
+        project_features = nn.Identity()
+
+        # conv as relative positional encoder
+        # TODO introduce Transformer-XL relative positional encoder
+        num_pos_layers = modality_cfg.conv_pos_depth
+        k = max(3, modality_cfg.conv_pos_width // num_pos_layers)
+
+        positional_encoder = nn.Sequential(
+            TransposeLast(),
+            *[
+                nn.Sequential(
+                    nn.Conv1d(
+                        embed_dim,
+                        embed_dim,
+                        kernel_size=k,
+                        padding=k // 2,
+                        groups=modality_cfg.conv_pos_groups,
+                    ),
+                    SamePad(k),
+                    TransposeLast(),
+                    LayerNorm(embed_dim, elementwise_affine=False),
+                    TransposeLast(),
+                    nn.GELU(),
+                )
+                for _ in range(num_pos_layers)
+            ],
+            TransposeLast(),
+        )
+
+        dpr = np.linspace(
+            modality_cfg.start_drop_path_rate,
+            modality_cfg.end_drop_path_rate,
+            modality_cfg.prenet_depth,
+        )
+
+        context_encoder = BlockEncoder(
+            nn.ModuleList(make_block(dpr[i]) for i in range(modality_cfg.prenet_depth)),
+            norm_layer(embed_dim) if not layer_norm_first else None,
+            layer_norm_first,
+            modality_cfg.prenet_layerdrop,
+            modality_cfg.prenet_dropout,
+        )
+
+        # TODO custom Decoder2d
+        decoder = (
+            Decoder1d(modality_cfg.decoder, embed_dim)
+            if modality_cfg.decoder is not None
+            else nn.Identity()
+        )
+
+        alibi_bias_fn = partial(
+            get_alibi_bias,
+            alibi_biases=alibi_biases,
+            heads=modality_cfg.num_alibi_heads,
+            dims=modality_cfg.alibi_dims,
+            distance=modality_cfg.alibi_distance,
+        )
+
+        super().__init__(
+            modality_cfg=modality_cfg,
+            embed_dim=embed_dim,
+            local_encoder=local_encoder,
+            project_features=project_features,
+            fixed_positional_encoder=None,
+            relative_positional_encoder=positional_encoder,
+            context_encoder=context_encoder,
+            decoder=decoder,
+            get_alibi_bias=alibi_bias_fn,
+        )
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        if self.decoder is not None:
+            self.decoder.reset_parameters()
+
+    @torch.no_grad()
+    def patchify(self, imgs):
+        """
+        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 *3)
+        """
+        p = self.modality_cfg.patch_size
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
+        x = torch.einsum("nchpwq->nhwpqc", x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 3))
+
+        return x
+
+    @torch.no_grad()
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = self.modality_cfg.patch_size
+        h = w = int(x.shape[1] ** 0.5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum("nhwpqc->nchpwq", x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
+
+    # TODO compute_mask 2d
+    # def compute_mask(
+    #     self,
+    #     x,
+    #     padding_mask,
+    #     mask_seed: Optional[MaskSeed],
+    #     apply,
+    #     shape=None,
+    #     precomputed_mask=None,
+    # ):
+    #     mlen = self.modality_cfg.mask_length
+    #     if mlen <= 1:
+    #         return super().compute_mask(
+    #             x, padding_mask, mask_seed, apply, precomputed_mask
+    #         )
+
+    #     if precomputed_mask is not None:
+    #         mask = precomputed_mask
+    #     else:
+    #         from fairseq.data.data_utils import compute_block_mask_2d
+
+    #         if shape is not None:
+    #             B, L, D = shape
+    #         else:
+    #             B, L, D = x.shape
+
+    #         mask = compute_block_mask_2d(
+    #             shape=(B, L),
+    #             mask_prob=self.modality_cfg.mask_prob,
+    #             mask_length=self.modality_cfg.mask_length,
+    #             mask_prob_adjust=self.modality_cfg.mask_prob_adjust,
+    #             inverse_mask=self.modality_cfg.inverse_mask,
+    #             require_same_masks=True,
+    #             mask_dropout=self.modality_cfg.mask_dropout,
+    #         )
+
+    #         # need to same device as x
+    #         mask = mask.to(device=x.get_device())
+
+    #     mask_info = self.make_maskinfo(x, mask, shape)
+    #     if apply:
+    #         x = self.apply_mask(x, mask_info)
+
+    #     return x, mask_info
+
+    def decoder_input(self, x, mask_info):
+        if (
+            not self.modality_cfg.transformer_decoder
+            or not self.modality_cfg.enc_dec_transformer
+        ):
+            return super().decoder_input(x, mask_info)
+
+        inp_drop = self.modality_cfg.decoder.input_dropout
+        if inp_drop > 0:
+            x = F.dropout(x, inp_drop, training=self.training, inplace=True)
+
+        kv = x[:, self.modality_cfg.num_extra_tokens :]
+
+        assert self.fixed_positional_encoder is not None
+        pos = self.fixed_positional_encoder(x, None).expand(x.size(0), -1, -1)
+
+        mask = mask_info.mask.bool()
+        if self.modality_cfg.decoder.add_positions_all:
+            kv = kv + pos[~mask].view(kv.shape)
+
+        q = pos[mask].view(x.size(0), -1, x.size(-1))
+
+        return q, kv
